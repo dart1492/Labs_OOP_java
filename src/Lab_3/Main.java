cdd02f342333f6d86662dd3666658f0734b92486
dart1492 <106056121+dart1492@users.noreply.github.com>
2022-09-02 22:34:38 +0300

    New beginning

---
+++ src/Lab_3/Main.java
@@ -0,0 +1,8 @@
+package Lab_3;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Lab_3");
+    }
+
+}
--- src/com/company/Lab_3.java
+++
@@ -1,5 +0,0 @@
-package com.company;
-
-public class Lab_3 {
-
-}
